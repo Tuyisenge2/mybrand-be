e166cfa0ac072127a5acea3484b97e6c6d71dd4a
Tuyisenge2 <tuyisengetito3@gmail.com>
2024-03-26 15:18:43 +0200

    Adding ts node globbally

--- src/document/server.ts
+++ src/document/server.ts
@@ -5,5 +5,10 @@ export default  {
         url: "http://localhost:5000/",
         description: "Local server",
       }
+      ,
+      {
+        url:"https://sore-jade-rooster.cyclic.app/",
+        description: "Online server",
+      }
     ],
   };
