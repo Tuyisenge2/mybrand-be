a5e8016c1a43ec34e13d37661f45d29491c1d5ad
Tuyisenge2 <tuyisengetito3@gmail.com>
2024-03-28 16:20:26 +0200

    configuring render on my localhost

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import docs from "./document";
 import express from "express";
 
 const app = express()
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(express.json()) // new
 
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs));
